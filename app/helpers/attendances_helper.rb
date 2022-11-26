f1a80b45ee451de013918fad2065e3af8b981ccb
toshi <toshi621015@gmail.com>
2022-11-26 06:43:42 +0000

    Finish add-user-attendances

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -7,7 +7,7 @@ module AttendancesHelper
       return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
     end
     # どれにも当てはまらなかった場合はfalseを返します。
-    false
+    return false
   end
 
   # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
